f942ffe906833339e8abad814cf09c102e10b1fb
danishPythonian1 <danishadam043@gmail.com>
2026-03-04 15:10:41 +0700

    first commit

---
+++ metnum_biseksi.py
@@ -0,0 +1,37 @@
+import sympy as sp
+import tabulate as tb
+
+x = sp.symbols('x')
+
+def metode_biseksi(function, a, b, tol):
+  data = []
+  headers = ["Iterasi", "a", "b", "c", "fc"]
+  f = sp.lambdify(x, function)
+  fa = f(a)
+  fb = f(b)
+  
+  if fa * fb >= 0:
+    return "Tidak mengandung akar"
+  
+  iterasi = 1
+  
+  while abs(b - a) > tol:
+    c = round((a + b)/2, 10)
+    fc = round(f(c), 10)
+    
+    data.append([iterasi, a, b, c, fc])
+    
+    if fc < 0 and fa < 0:
+      fa = fc
+      a = c
+    else:
+      fb = fc
+      b = c
+    
+    iterasi += 1
+  print(tb.tabulate(data, headers = headers, tablefmt="psql"))
+  return f"Nilai c mendekati akar: {c}"
+        
+# aa = metode_biseksi(x**3 - x - 1, 1, 2, 0.000001)
+
+# print(aa)
---
+++ metnum_regulafalsi.py
@@ -0,0 +1,39 @@
+import sympy as sp
+import tabulate as tb
+
+x = sp.symbols('x')
+
+def metode_regulaFalsi(function, a, b, tol):
+  f = sp.lambdify(x, function)
+  fa = f(a)
+  fb = f(b)
+  
+  if fa * fb >= 0:
+    return "Tidak mengandung akar"
+  c = b - ((fb*(b-a))/(fb-fa))
+  fc = f(c)
+  
+  iterasi = 1
+  
+  headers = ["iterasi", "a", "b", "c", "fc"]
+  data = [[iterasi, a, b, c, fc]]
+  while abs(fc) > tol:
+    if fc < 0 and fa < 0:
+      fa = fc
+      a = c
+    else:
+      fb = fc
+      b = c
+    c = b - ((fb*(b-a))/(fb-fa))
+    fc = f(c)
+    
+    iterasi += 1
+    
+    data.append([iterasi, a, b, c, fc])
+  tabelData = tb.tabulate(data, headers = headers, tablefmt="psql")
+  print(tabelData)
+  print(f"Nilai c mendekati akar: {c}")
+  print(f"Nilai fc: {fc}")
+  return ""
+
+# print(metode_regulaFalsi(x**3 - x, -1, 1, 0.0001))
